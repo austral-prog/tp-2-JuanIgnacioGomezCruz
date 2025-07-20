eea7ac809606a543b9b0ca77d1a72a63ffbd221e
JuanIgnacioGomezCruz <jigomezcruz@mail.austral.edu.ar>
2025-07-20 17:58:44 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,7 @@
 def ada():
-    first_name = "AdA"
-    last_name = "LoVeLAce"
+    first_name = "ada"
+    last_name= "lovelace"
+    print(first_name + " " + last_name)
+    print(first_name.title() + " " + last_name.title())
+    print(first_name.upper() + " " + last_name.upper()) 
+    print(f"\t{( first_name + " " + last_name).rstrip()}")
